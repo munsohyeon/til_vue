048ce9a9ddf9656ca9cddba153d0b8582fda6fa0
munsohyeon <msh1787@gmail.com>
2025-06-18 14:18:51 +0900

    [feat]day 01 first vue

---
+++ src/components/RearWheel.vue
@@ -0,0 +1,21 @@
+<script setup></script>
+import Tire from "@/components/Tire.vue";
+
+<template>
+  <div class="rear-wheel">
+    <span>REAR WHEEL</span>
+    <Tire color="BLUE"/> 
+  </div>
+</template>
+
+<style scoped>
+.rear-wheel {
+  border: 1px solid black;
+  border-radius: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 150px;
+  height: 150px;
+}
+</style>
---
+++ src/components/Tire.vue
@@ -0,0 +1,15 @@
+<script setup>
+// Data coming in from outside props.
+const props = defineProps({
+  color: String// disgnate th type
+});
+</script>
+
+<template>
+  <!-- ":style" This is a vue grammer
+        "{{}}" Brace is two that are overlapped (Mustache templet)
+  -->
+  <div :style="{color: props.color}">{{props.color || 'BLACK'}}TIRE</div>
+</template>
+
+
