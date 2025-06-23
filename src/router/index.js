871e79c98c40cb8004b4278be566865b2e9fd819
munsohyeon <msh1787@gmail.com>
2025-06-23 09:43:01 +0900

    [docs] vue 3day

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,9 @@
-import { createRouter, createWebHistory } from "vue-router";
-import Mustache from "@/views/Mustache.vue";
-import Component from "@/views/Component.vue";
+import { createRouter, createWebHistory } from 'vue-router'
+import Component from '@/views/Component.vue';
+import Mustache from '@/views/Mustache.vue';
+import Chapter3 from '@/views/Chapter3.vue';
+import Chapter4 from '@/views/Chapter4.vue';
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -12,8 +15,16 @@ const router = createRouter({
     {
       path: '/mustache',
       component: Mustache
+    },
+    {
+      path: '/chapter3',
+      component: Chapter3
+    },
+    {
+      path: '/chapter4',
+      component: Chapter4
     }
-  ],
-});
+  ]
+})
 
-export default router;
+export default router
---
+++ src/views/Chapter5.vue
@@ -0,0 +1,12 @@
+<script setup>
+let pushUp = 0;
+</script>
+
+<template>
+  <h1>Chapter5.vue - 반응형 상태</h1>
+  <hr>
+  <div>팔 굽혀 펴기: {{ pushUp }}</div>
+  <hr>
+</template>
+
+<style scoped></style>
